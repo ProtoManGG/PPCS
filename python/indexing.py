e9a309e263b21c73547aeddee1956e1f56eb7521
mananarora200 <mananarora200@gmail.com>
2020-08-26 15:36:26 +0530

    indexing complete

--- python/indexing.py
+++ python/indexing.py
@@ -1,12 +1,12 @@
 import pandas as pd 
 import psycopg2
 connection = psycopg2.connect(host="localhost", port=5432,
-                        database="post-pandemic-db", user="postgres", password="abhay1234@")
+                        database="post-pandemic-db", user="postgres", password="7878")
 cursor = connection.cursor()
 cursor.execute("DROP TABLE IF EXISTS COVID19_HOTSPOTS")
 sql ='''CREATE TABLE COVID19_HOTSPOTS(
    Date DATE,
-   Name_of_State_UT VARCHAR(20),
+   Name_of_State_UT VARCHAR(40),
    Latitude float,
    Longitude float,
    Total_Confirmed_cases int,
@@ -18,8 +18,8 @@ sql ='''CREATE TABLE COVID19_HOTSPOTS(
 )'''
 cursor.execute(sql)
 df = pd.read_csv('python/complete.csv')
-a = list(df.iloc()[0])
-for i in range(list(df.count())[0]):
-    query = '''INSERT INTO covid19_hotspots (Date, Name_of_State_UT,Latitude, Longitude, Total_Confirmed_cases, Death, Cured_Discharged_Migrated, New_cases, New_deaths, New_recovered ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
-    cursor.execute(query,(list(df.iloc()[i])[0],list(df.iloc()[i])[1],list(df.iloc()[i])[2],list(df.iloc()[i])[3],list(df.iloc()[i])[4],list(df.iloc()[i])[5],list(df.iloc()[i])[6],list(df.iloc()[i])[7],list(df.iloc()[i])[8],list(df.iloc()[i])[9]))
-cursor.commit()
+data_list = [list(row) for row in df.itertuples(index=False)] 
+for i in data_list:
+    query = "INSERT INTO COVID19_HOTSPOTS (Date, Name_of_State_UT,Latitude, Longitude, Total_Confirmed_cases, Death, Cured_Discharged_Migrated, New_cases, New_deaths, New_recovered ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(query,tuple(i))
+connection.commit()
