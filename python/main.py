753d63554cf60c1fe9b32645983524eb3c928b9d
mananarora200 <mananarora200@gmail.com>
2020-08-31 00:14:26 +0530

    return hotspots successfully

--- python/main.py
+++ python/main.py
@@ -1,17 +1,28 @@
-from flask import Flask,request
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import uvicorn
+import psycopg2
+# import reverse_geocoder as rg
+connection = psycopg2.connect(host="localhost", port=5432,
+                        database="post-pandemic-db", user="postgres", password="7878")
+cursor = connection.cursor()
+app = FastAPI(debug=True)
 
-flask_object = Flask(__name__)
+class Loc(BaseModel):
+    lat: float
+    long: float
+# def find_state(lat,long):
+#     return rg.search((lat,long))[0]['name']
 
-
-@flask_object.route('/', methods=['POST'])
-def index():
-    data = request.json()
-    if data:
-        print(data)
-        return 'Done'
-    else:
-        return 'No data found'
-
+@app.post('/')
+async def index(loc : Loc):
+    # state = find_state(loc.lat,loc.long)
+    query = f"SELECT Latitude,Longitude FROM COVID19_HOTSPOTS WHERE (Latitude BETWEEN {loc.lat-0.2} AND {loc.lat+0.2}) AND (Longitude BETWEEN {loc.long-0.2} AND {loc.long+0.2})"
+    cursor.execute(query)
+    loc_data = cursor.fetchall()
+    print(len(loc_data))
+    return loc_data
 
 if __name__ == "__main__":
-    flask_object.run(debug=True)
+    uvicorn.run(app,host = '0.0.0.0',port = 8000)
