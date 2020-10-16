a93ac3fb342421c30ee81008d623aec04a0cd71c
manan arora <mananarora200@gmail.com>
2020-10-16 14:39:15 +0530

    new api created

--- python/main.py
+++ python/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Header
 from pydantic import BaseModel
 import json
+from typing import Optional,List
 import uvicorn
 import psycopg2
-# import reverse_geocoder as rg
+import pandas as pd
+import numpy as np 
+from sklearn.cluster import KMeans
+
 connection = psycopg2.connect(host="localhost", port=5432,
                         database="post-pandemic-db", user="postgres", password="7878")
 cursor = connection.cursor()
@@ -11,18 +15,37 @@ app = FastAPI(debug=True)
 
 class Loc(BaseModel):
     lat: float
-    long: float
-# def find_state(lat,long):
-#     return rg.search((lat,long))[0]['name']
+    longi: float
 
-@app.post('/')
+@app.post('/update')
+async def update():
+    # query = f"UPDATE User_Data SET lat = %s and long = %s where"
+    pass
+
+@app.post('/hotspot/')
 async def index(loc : Loc):
-    # state = find_state(loc.lat,loc.long)
-    query = f"SELECT Latitude,Longitude FROM COVID19_HOTSPOTS WHERE (Latitude BETWEEN {loc.lat-0.2} AND {loc.lat+0.2}) AND (Longitude BETWEEN {loc.long-0.2} AND {loc.long+0.2})"
+    print(loc)
+    query = f"SELECT lat,long,death,active,recovered FROM Hotspot WHERE (lat BETWEEN {loc.lat-0.2} AND {loc.lat+0.2}) AND (long BETWEEN {loc.longi-0.2} AND {loc.longi+0.2})"
     cursor.execute(query)
     loc_data = cursor.fetchall()
     print(len(loc_data))
-    return loc_data
+    return_data = {}
+    for j,i in enumerate(loc_data):
+        return_dict = {}
+        return_dict["lat"] = i[0]
+        return_dict["long"] = i[1]
+        return_dict["death"] = i[2]
+        return_dict["active"] = i[3]
+        return_dict["recovered"] = i[4]
+        return_data[f"hotspot {j+1}"] = return_dict
+    print(return_data)
+    sql = f'''SELECT lat,long FROM User_Data WHERE (lat BETWEEN {loc.lat-0.2} AND {loc.lat+0.2}) AND (long BETWEEN {loc.longi-0.2} AND {loc.longi+0.2})'''
+    cursor.execute(sql)
+    crowd_data = cursor.fetchall()
+    kmean=KMeans(n_clusters=10)
+    kmean.fit(crowd_data)
+    print(kmean.cluster_centers_)
+    return {"corona_hotspot":return_data,"crowd_hotspot":kmean.cluster_centers_}
 
 if __name__ == "__main__":
     uvicorn.run(app,host = '0.0.0.0',port = 8000)
--- python/new_api.py
+++ python/new_api.py
@@ -2,23 +2,34 @@ from datetime import datetime, timedelta
 from typing import Optional
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
+from sklearn.cluster import KMeans
+from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
-import crypt
 
-connection = psycopg2.connect(host="192.168.18.58", port=5432,
-                            database="post-pandemic-db", user="postgres", password="abhay1234@")
+connection = psycopg2.connect(host="localhost", port=5432,
+                            database="post-pandemic-db", user="postgres", password="7878")
 cursor = connection.cursor()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl= 'token')
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_HOURS = 10
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -26,44 +37,34 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-
 class User(BaseModel):
     username: str
-    address: str
     phone_no: int
     password: str
     email: Optional[str] = None
+    lat: float
+    longi : float
     disabled: Optional[bool] = None
 
-class UserInDB(User):
-    hashed_password: str
+class Login_user(BaseModel):
+    email : str
+    password : str
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+class Action(BaseModel):
+    lat : float
+    longi : float
+    access_token: str
 
 def add_user(user_tuple):
-    sql = "INSERT INTO User_Data (username,address,phone_no,password,email) VALUES(%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO User_Data (username,phone_no,password,email,lat,long) VALUES(%s,%s,%s,%s,%s,%s)"
     cursor.execute(sql,user_tuple)
+    connection.commit()
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return pwd_context.hash(password)
-
-def authenticate_user(users_db, username: str, password: str):
-    user = get_user(users_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -77,33 +78,88 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 
 @app.post('/signup',response_model = Token)
 async def signup_get_token(user:User):
-    user_tuple = (user.username,user.address,user.phone_no,crypt(user.password,gen_salt = 'md5'),user.email)
-    add_user(user_tuple)
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-@app.post("/login", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    cursor.execute("SELECT username FROM User_Data")
-    results = cursor.fetchall()
-    users_db = {}
-    for row in results:
-        users_db["username"] = row['username']
-    user = authenticate_user(users_db, form_data.username, form_data.password)
-    if not user:
+    cursor.execute(f"SELECT email FROM User_Data WHERE email = '{user.email}'")
+    user_db = cursor.fetchall()
+    if user_db  != []:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            status_code=status.HTTP_306_RESERVED,
+            detail="Email is already registered !!",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    user_tuple = (user.username,user.phone_no,get_password_hash(user.password),user.email,user.lat,user.longi)
+    add_user(user_tuple)
+    access_token_expires = timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.email}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "Bearer"}
 
+@app.post("/login", response_model=Token)
+async def login_for_access_token(form_data: Login_user):
+    cursor.execute(f"SELECT email,password FROM User_Data WHERE email = '{form_data.email}'")
+    user_db = cursor.fetchall()
+    if user_db  == []:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    if verify_password(form_data.password,user_db[0][1]) == False:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
+    access_token = create_access_token(
+        data={"sub": form_data.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "Bearer"}
+
+@app.post("/covid")
+async def get_covid_hotspot(action : Action):
+    user = jwt.decode(action.access_token,key=SECRET_KEY,algorithms=ALGORITHM)
+    cursor.execute(f"SELECT email FROM User_Data WHERE email = '{user['sub']}'")
+    user_db = cursor.fetchall()
+    if user_db  == []:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not Authorized",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    sql_update = f"UPDATE User_Data SET lat = {round(action.lat,4)},long = {round(action.longi,4)} WHERE email = '{user['sub']}'"
+    cursor.execute(sql_update)
+    connection.commit()
+    query = f"SELECT lat,long,death,active,recovered FROM Hotspot WHERE (lat BETWEEN {action.lat-0.05} AND {action.lat+0.05}) AND (long BETWEEN {action.longi-0.05} AND {action.longi+0.05})"
+    cursor.execute(query)
+    loc_data = cursor.fetchall()
+    return_data = []
+    if loc_data!= []:
+        for i in loc_data:
+            return_dict = {}
+            return_dict["lat"] = i[0]
+            return_dict["long"] = i[1]
+            return_dict["death"] = i[2]
+            return_dict["active"] = i[3]
+            return_dict["recovered"] = i[4]
+            return_data.append(return_dict)
+    sql = f'''SELECT lat,long FROM User_Data WHERE (lat BETWEEN {action.lat-0.5} AND {action.lat+0.5}) AND (long BETWEEN {action.longi-0.5} AND {action.longi+0.5})'''
+    cursor.execute(sql)
+    crowd_data = cursor.fetchall()
+    crowd_data = []
+    if crowd_data!=[]:
+        kmean=KMeans(n_clusters=10)
+        kmean.fit(crowd_data)
+        crowd_data = kmean.cluster_centers_.tolist()
+    access_token_expires = timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
+    access_token = create_access_token(
+        data={"sub": user['sub']}, expires_delta=access_token_expires
+    )
+    print(return_data,len(return_data))
+    print("")
+    print("")
+    print(crowd_data,len(crowd_data))
+    return {"corona_hotspot":return_data,"crowd_hotspot":crowd_data,"access_token":access_token}
+    
 if __name__ == "__main__":
     uvicorn.run(app,host = '0.0.0.0', port = 8000)
