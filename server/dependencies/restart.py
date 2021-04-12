c98974cb7dc57d331f8cc10b39ef32d668a3f2da
mananarora200 <mananarora200@gmail.com>
2021-04-12 10:06:38 +0530

    docker-compose

--- server/dependencies/restart.py
+++ server/dependencies/restart.py
@@ -7,7 +7,7 @@ connection = psycopg2.connect(host="localhost", port=5432,
                             database="post-pandemic-db", user="postgres", password="7878")
 cursor = connection.cursor()  
 
-admin_client = KafkaAdminClient(bootstrap_servers="dockervm:9092")
+admin_client = KafkaAdminClient(bootstrap_servers="localhost")
 
 topic_list = []
 topic_list.append(NewTopic(name='get-hotspot-in', num_partitions=1, replication_factor=1))
