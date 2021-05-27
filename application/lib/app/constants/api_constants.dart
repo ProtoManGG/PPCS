a1432881e48ab0662d4ac0f209e314cef5c59d4b
ProtoManGG <52622543+ProtoManGG@users.noreply.github.com>
2021-05-27 23:24:17 +0530

    added support for ios

--- application/lib/app/constants/api_constants.dart
+++ application/lib/app/constants/api_constants.dart
@@ -1,3 +1,3 @@
 // const String baseUrl = "http://192.168.0.108:8000";
-const String baseUrl = "https://7d765655931d.ngrok.io/";
+const String baseUrl = "https://d457db522001.ngrok.io/";
 const kGoogleApiKey = "AIzaSyC65uWOQsZSDpSfeDyKLIYWorwADgRu5RU";
